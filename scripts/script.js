41314b485ef1fb96f3df200cd05fa03023779772
SARKOR_A <abilkosimovsarkor@gmail.com>
2024-02-08 18:31:46 +0500

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+console.log('Welcome');
+console.log('Developed by Sarkor');
+
+
+
+// const burgerOpen = document.querySelector('.nav__open');
+// const burgerClose = document.querySelector('.nav__exit-btn');
+// const burgerMenu = document.querySelector('.header-content');
+// const body = document.querySelector('body');
+// const filter = document.querySelector('.filter');
+
+// burgerOpen.addEventListener('click', () =>{
+//     burgerMenu.classList.add('active')
+//     body.classList.add('active')
+//     filter.classList.add('active')
+// })
+
+// burgerClose.addEventListener('click', () =>{
+//     burgerMenu.classList.remove('active')
+//     body.classList.remove('active')
+//     filter.classList.remove('active')
+// })
+
+// filter.addEventListener('click', () =>{
+//     burgerMenu.classList.remove('active')
+//     body.classList.remove('active')
+//     filter.classList.remove('active')
+// })
